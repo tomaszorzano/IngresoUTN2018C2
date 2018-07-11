1be7b8a303dfc9f9ef1d7bfd442f9a3b0e09f6cf
tomaszorzano <40808662+tomaszorzano@users.noreply.github.com>
2018-07-11 20:59:34 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,17 @@
 function mostrar()
-{
+{ var numero1;
+  var numero2;
+  var resta;
+             numero1=prompt("Ingrese un primer numero aqui");
+                     numero1=parseInt(numero1);
+             numero2=prompt("Ingrese un segundo numero aqui");
+                     numero2=parseInt(numero1);
+             if (numero1 == numero2)
+             {    alert("el numero es " + numero1 ) }
+             else {
+                   if (numero1>numero2)
+                   { resta=numero1-numero2}
+             }        
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
-{
+{   var ancho;
+    var largo;
+    var perimetro;
+
+    ancho=prompt( " Ingrese Ancho del rectangulo "  );
+         ancho=parseInt(ancho);
+    largo=prompt( " Ingrese largo del rectangulo " );
+         largo=parseInt(largo);
+    perimetro= 2*(ancho + largo) 
+
+    aler(" El perimetro es " perimetro )     
 
 }
