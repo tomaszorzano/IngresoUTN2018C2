ac275aabcf9b291237c0c91c117b8a70580fc94b
tomaszorzano <40808662+tomaszorzano@users.noreply.github.com>
2018-07-11 22:02:14 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,7 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño){
 
+    case "Enero":
+                  alert("Que Comiencen bien el año");
+         break;         
+    case "Marzo":
+                  alert("A clases");
+         break;
+    case "Julio":               
+                  alert("Se viene las vacaciones");
+         break;
+    case "Diciembre":
+                  alert("Felices Fiestas");
+         break;                        
+
+                  }
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,8 +2,28 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
+ switch (mesDelAño) {
+       case "Enero":
+       case "Febrero":
+       case "Marzo":
+       case "Abril":
+       case "Mayo":
+       case "Junio": 
+                    alert("Falta para el Invierno");
+             break;                     
+       
+       case "Julio":
+       case "Agosto":
+                    alert("Estamos en invierno");
+              break;        
+      
+      case "Septiembre":
+      case "Octubre":
+      case "Noviembre":
+      case "Diciembre":
+                       alert("Ya pasamos el frio, ahora calor!!!.");
+              break;                                                   
+                    }
 
 
 
